c6ee658d4b906789d1ad5718d9bd6c1c28cec025
Stas Sytnykov <stas82kak05@gmail.com>
2023-03-21 16:27:47 +0200

    added post request to mongoDB

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Product = require('./models/productModels');
 require('dotenv').config();
 
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
 //routes
 app.get('/', (req, res) => {
   res.send('Hello!');
+});
+
+app.post('/product', async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
 });
 
 mongoose
