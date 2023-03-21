93128d7d2b60010ff7c7e325038f194e4739e75f
Stas Sytnykov <stas82kak05@gmail.com>
2023-03-21 16:09:43 +0200

    created product model

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const port = 3000;
 
 //routes
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello!');
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+mongoose
+  .connect(
+    `mongodb+srv://stas82kak05:${process.env.MONGOOSE_DATABASE_PASS}@cluster0.pfdpdd6.mongodb.net/CRUD-API?retryWrites=true&w=majority`,
+  )
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(port, () => {
+      console.log(`Example app listening on port ${port}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
